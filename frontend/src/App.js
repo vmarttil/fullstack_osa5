851993745044e5e645f908a22b2611b2a85fab4c
Ville Marttila <ville.marttila@iki.fi>
2020-02-23 22:49:29 +0200

    Tehtävä 5.2 tehty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -25,12 +34,21 @@ const App = () => {
         username, password,
       })
 
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
 
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
    
 const loginForm = () => (
@@ -40,7 +58,7 @@ const loginForm = () => (
 const blogList = () => {
   return (
     <div>
-      <p>{user.name} logged in </p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
     )
