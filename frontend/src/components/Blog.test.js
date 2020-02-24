6a850aabd760ea05ddd42cf99824737ac36bf041
Ville Marttila <ville.marttila@iki.fi>
2020-02-24 21:43:10 +0200

    Tehtävä 5.15 tehty

--- frontend/src/components/Blog.test.js
+++ frontend/src/components/Blog.test.js
@@ -3,25 +3,24 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
+const blog = {
+  title: 'React patterns',
+  author: 'Michael Chan',
+  url: 'https://reactpatterns.com/',
+  likes: 7,
+  user: '5e4a93ec7abfc719d02658ae'
+}
+
+const user = {
+  username: 'vmarttil',
+  name: 'Ville Marttila',
+  id: '5e4a93ec7abfc719d02658ae'
+}
+
 test('renders correct content', () => {
-  const blog = {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    user: '5e4a93ec7abfc719d02658ae'
-  }
-
-  const user = {
-    username: 'vmarttil',
-    name: 'Ville Marttila',
-    id: '5e4a93ec7abfc719d02658ae'
-  }
-
   const component = render(
     <Blog blog={blog} user={user} updateBlog={jest.fn()} deleteBlog={jest.fn()}/>
   )
-
   expect(component.container.textContent).toContain('React patterns')
   expect(component.container.textContent).toContain('Michael Chan')
   expect(component.container.textContent).not.toContain('https://reactpatterns.com/')
@@ -29,30 +28,28 @@ test('renders correct content', () => {
 })
 
 test('clicking the view button allows the url and the likes to be rendered', async () => {
-  const blog = {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    user: '5e4a93ec7abfc719d02658ae'
-  }
-
-  const user = {
-    username: 'vmarttil',
-    name: 'Ville Marttila',
-    id: '5e4a93ec7abfc719d02658ae'
-  }
-
   const component = render(
     <Blog blog={blog} user={user} updateBlog={jest.fn()} deleteBlog={jest.fn()}/>
   )
-
   const button = component.getByText('view')
   fireEvent.click(button)
-
   expect(component.container.textContent).toContain('React patterns')
   expect(component.container.textContent).toContain('Michael Chan')
   expect(component.container.textContent).toContain('https://reactpatterns.com/')
   expect(component.container.textContent).toContain('likes 7')
+})
+
+
+test('clicking the like button twice calls the updateBlog function twice', async () => {
+  const mockUpdateBlog = jest.fn()
+  const component = render(
+    <Blog blog={blog} user={user} updateBlog={mockUpdateBlog} deleteBlog={jest.fn()}/>
+  )
+  const viewButton = component.getByText('view')
+  fireEvent.click(viewButton)
+  const likeButton = component.getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+  expect(mockUpdateBlog.mock.calls.length).toBe(2)
 })
 
