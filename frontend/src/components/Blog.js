408c50faa08a03601e2c24eac17abd5990576164
Ville Marttila <ville.marttila@iki.fi>
2020-02-24 05:40:54 +0200

    Tehtävä 5.7 tehty

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,8 +1,26 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [details, setDetails] = useState(false)
+
+  const showWhenDetails = { display: details ? '' : 'none' }
+  const hideWhenDetails = { display: details ? 'none' : '' }
+
+  const toggleDetails = () => {
+    setDetails(!details)
+  }
+
+  return (
+    <div className="blogEntry">
+      '{blog.title}' by {blog.author} 
+      <button style={hideWhenDetails} onClick={toggleDetails}>view</button>
+      <button style={showWhenDetails} onClick={toggleDetails}>hide</button>
+      <div style={showWhenDetails}>
+        {blog.url}<br/>
+        likes {blog.likes}<button>like</button><br/>
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
